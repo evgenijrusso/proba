ad41b11929bfa276362dff1de6babc02a7cdf48c
Rasen Nikov <rasen800@gmail.com>
2024-02-16 19:24:52 +0600

    Регистрация

--- Example/another/urls.py
+++ Example/another/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from Example.another.views import TaskListView, MessageListView
+from another.views import TaskListView, MessageListView
 
 urlpatterns = [
     path('another/', TaskListView.as_view(), name='tasks'),
